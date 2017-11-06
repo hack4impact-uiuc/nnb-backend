7bb09cf075ecbdc06b0be40161b91cb0539ffc43
shreyshrey1 <talk2shreyas@gmail.com>
2017-11-05 23:37:28 -0600

    Adding tables to database in progress

--- api/models.py
+++ api/models.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy.dialects.postgresql import JSON
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -21,9 +20,10 @@ class PointsOfInterest(db.Model):
     name = db.Column(db.String, nullable=False)
     data = db.Column(db.Date, nullable=False)
     eventinfo = db.Column(db.String, nullable=False)
-    year = db.Column(db.Int, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
     x_coord = db.Column(db.Float, nullable=False)
     y_coord = db.Column(db.Float, nullable=False)
+    years = db.relationship('Maps', backref='poi', Lazy=True)
 
     def __init__(self, id, name, data, eventinfo, year, x_coord, y_coord):
         self.id = id
@@ -44,7 +44,13 @@ class maps(db.Model):
     __tablename__ = "maps"
 
     image_url = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, db.ForeignKey('poi.id'), nullable=False)
 
+    # Not sure if I did the foreign key stuff correctly ^^^
+
+    def __init__(self, image_url, year):
+        self.image_url = image_url
+        self.year = year
 
 
 
