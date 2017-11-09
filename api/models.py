c8a185ba56818a8c209eec45bb8566412741e94c
CodeBrew28 <arshiam2@illinois.edu>
2017-11-08 20:01:01 -0600

    added routes for pois and fixed bugs in models

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
+from sqlalchemy import func
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -22,8 +23,8 @@ class PointsOfInterest(db.Model):
     year = db.Column(db.Integer, nullable=False)
     x_coord = db.Column(db.Float, nullable=False)
     y_coord = db.Column(db.Float, nullable=False)
-    years = db.relationship('Maps', backref='poi', lazy=True)
-    content = db.relationship('Content', backref='poi', lazy=True)
+    # years = db.relationship('Maps', backref='poi', lazy=True)
+    # content = db.relationship('Content', backref='poi', lazy=True)
 
     def __init__(self, id, name, data, eventinfo, year, x_coord, y_coord):
         self.id = id
@@ -38,17 +39,16 @@ class PointsOfInterest(db.Model):
         return '<name {}>'.format(self.name)
 
     def toDict(self):
-        return {'id': self.id, 'name': self.name}
+        return {'id': self.id, 'name': self.name, 'data': self.data}
 
 
-class maps(db.Model):
+
+class Maps(db.Model):
     __tablename__ = "maps"
     
     id = db.Column(db.Integer, unique=True, primary_key=True)
     image_url = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, db.ForeignKey('poi.id'), nullable=False)
-
-    # Not sure if I did the foreign key stuff correctly ^^^
 
     def __init__(self, image_url, year):
         self.id = id
@@ -62,7 +62,7 @@ class maps(db.Model):
         return {'year': self.year, 'image_url': self.image_url}
 
 
-class content(db.Model):
+class Content(db.Model):
     __tablename__ = "content"
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
@@ -70,7 +70,7 @@ class content(db.Model):
     caption = db.Column(db.String, nullable=True)
     poi_id = db.Column(db.Integer, db.ForeignKey('poi.id'), nullable=False)
 
-    def __init__(self, id, content_url, caption, poi_id):
+    def __init__(self,content_url, caption, poi_id):
         self.id = id
         self.content_url = content_url
         self.caption = caption
@@ -84,14 +84,14 @@ class content(db.Model):
                 self.content_url, 'caption': self.caption}
 
 
-class story_names(db.Model):
+class StoryNames(db.Model):
     __tablename__ = 'story_names'
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
     story_name = db.Column(db.String, nullable=0)
     story_id = db.relationship('Stories', backref='story_name', lazy=True)
 
-    def __init__(self, story_name, story_id):
+    def __init__(self, id, story_name, story_id):
         self.id = id
         self.story_name = story_name
         self.story_id = story_id
@@ -100,18 +100,17 @@ class story_names(db.Model):
         return '<story_names {}>'.format(self.story_name)
 
     def toDict(self):
-        return {'id': self.id, 'story_name': self.story_name, 
-                'story_id': self.story_id}
+        return {'id': self.id, 'story_name': self.story_name}
 
 
-class stories(db.Model):
+class Stories(db.Model):
     __tablename__ = 'stories'
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
     story_uuid = db.Column(db.Integer, db.ForeignKey("story_names.id"), nullable=False)
     poi_id = db.Column(db.Integer, db.ForeignKey('poi.id'), nullable=False)
 
-    def __init__(self, story_uuid, poi_id):
+    def __init__(self, id, story_uuid, poi_id):
         self.id = id
         self.story_uuid = story_uuid
         self.poi_id = poi_id
@@ -123,17 +122,17 @@ class stories(db.Model):
         return {'story_uuid': self.story_uuid, 'poi_id': self.poi_id}
 
 
-class additional_links(db.Model):
+class AdditionalLinks(db.Model):
     __tablename__ = 'additional_links'
 
-    id = db.Column(db.Integer, unique=True, primary_key=True)
+    id = db.Column (db.Integer, unique=True, primary_key=True)
     url = db.Column(db.String)
     poi_id = db.Column(db.Integer, db.ForeignKey('poi.id'), nullable=False)
 
-    def __init__(self, url, poi_id):
-        self.id = id
-        self.url = url
-        self.poi_id = poi_id
+    # def __init__(self,id, url, poi_id):
+    #     self.id = id
+    #     self.url = url
+    #     self.poi_id = poi_id
 
     def __repr__(self):
         return '<additional_links poi_id = {}>'.format(self.poi_id)
--- api/views/stories.py
+++ api/views/stories.py
@@ -2,15 +2,45 @@ from api import app
 from flask import Blueprint, request
 from .. import db
 from api.models import PointsOfInterest
-
+import json
+from flask import jsonify
+from api.utils import serializeList
+from api.models import StoryNames, Stories
+from sqlalchemy import func
 mod = Blueprint('stories', __name__)
 
 #Returns all story names
 @app.route('/stories', methods=['GET'])
 def stories():
-    return "All story names"
+    return jsonify(serializeList(StoryNames.query.all()))
 
 #Returns all POIS for a specific Story
-@app.route('/stories/<input>', methods=['GET', 'POST'])
-def stories2(input):
-    return "All POIs for story: " + input
+@app.route('/stories/<inputStoryName>', methods=['GET'])
+def stories_get(inputStoryName):
+    return jsonify(serializeList((Stories.query.filter(func.lower(StoryNames.story_name)==func.lower(inputStoryName)))))
+
+#adds a POI to an existing story
+@app.route('/story_poi/')
+def story_point(input_id, input_story_uuid, input_poi_id):
+    # story_added = Stories(
+    #     id = input_id,
+    #     story_uuid =  input_story_uuid,
+    #     poi_id = input_poi_id
+    # )
+    # db.session.add(story_added)
+    # db.session.commit()
+    return "new story poi added to existing story"
+
+#adds a new story
+
+@app.route('/story', methods=["POST"])
+def new_story():
+    # json_dict = json.loads(request.data)
+    # story_added = StoryNames(
+    #     id = 1,
+    #     story_name =  json_dict['story_name'],
+    #     story_id = json_dict['story_id'],
+    # )
+    # db.session.add(story_added)
+    # db.session.commit()
+    return "new story added"
