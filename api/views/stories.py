50801d93e507d1e383ae8421202ed85ea9a14d62
Timothy Ko <timothy.l.ko@gmail.com>
2017-11-12 23:06:24 -0600

    stories standardized

--- api/views/stories.py
+++ api/views/stories.py
@@ -8,30 +8,35 @@ from api.utils import serializeList
 from sqlalchemy import func
 mod = Blueprint('stories', __name__)
 
-#Returns all story names
+# Returns all story names
 @app.route('/stories', methods=['GET'])
 def stories():
-    return jsonify(serializeList(StoryNames.query.all()))
+    try:
+        return jsonify({'status': 'success', 'data': serializeList(StoryNames.query.all())})
+    except Exception as ex:
+        return jsonify({'status': 'failed', 'message': ex})
 
-#Returns all POIS for a specific Story
+# Returns all POIS for a specific Story
 @app.route('/stories/<inputStoryName>', methods=['GET'])
 def stories_get(inputStoryName):
-    return jsonify(serializeList((Stories.query.filter(func.lower(StoryNames.story_name)==func.lower(inputStoryName)))))
+    try:
+        return jsonify({'status': 'success', 'data': serializeList((Stories.query.filter(func.lower(StoryNames.story_name)==func.lower(inputStoryName))))})
+    except Exception as ex:
+        return jsonify({'status': 'failed', 'message': ex})
 
-#adds a POI to an existing story, POI must exist!!!!
+# adds a POI to an existing story, POI must exist!!!!
 @app.route('/story_poi', methods=['POST'])
 def story_point():
     if request.method == "POST":
         json_dict = json.loads(request.data)
-        
         try:
             storynames = StoryNames.query.get(json_dict["input_story_name_id"]) #check if it is empty later
             storynames.story_id.append(Stories()) #add new Stories() to storynames
             
             db.session.commit()
-            #get poi and add the same story to it
+            # get poi and add the same story to it
             poi = PointsOfInterest.query.filter(PointsOfInterest.id==json_dict["input_poi_id"])[0]
-            poi.stories.append(storynames.story_id[-1]) #gets last index, which is the Stories() that was just added
+            poi.stories.append(storynames.story_id[-1]) # gets last index, which is the Stories() that was just added
             db.session.commit()
         except Exception as ex:
             return jsonify({"status": "failed", "message": str(ex)})
@@ -42,10 +47,13 @@ def story_point():
 #adds a new story
 @app.route('/story', methods=["POST"])
 def new_story():
-    json_dict = json.loads(request.data)
-    story_added = StoryNames(
-        story_name = json_dict['story_name'],
-    )
-    db.session.add(story_added)
-    db.session.commit()
-    return "new story added" 
+    try:
+        json_dict = json.loads(request.data)
+        story_added = StoryNames(
+            story_name=json_dict['story_name'],
+        )
+        db.session.add(story_added)
+        db.session.commit()
+        return jsonify({'status': 'success', 'message': 'Added new Story'})
+    except Exception as ex:
+        return jsonify({'status':'failed','message':ex})
