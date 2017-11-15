119fbf0803c3012f243b68c63e2d7051b39b77b7
Timothy Ko <timothy.l.ko@gmail.com>
2017-11-15 01:21:15 -0600

    added CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
+CORS(app)
 
 db = SQLAlchemy(app)
 db.create_all()
