ec0d4679debf4771aaaab08818071584c9f687d5
Timothy Ko <timothy.l.ko@gmail.com>
2017-11-21 20:52:04 -0800

    added authentication endpoints

---
+++ api/views/auth.py
@@ -0,0 +1,24 @@
+from api import app
+from flask import Blueprint, request
+from api.models import PointsOfInterest
+from .. import db
+import json
+from api.utils import serializeList
+from flask import jsonify
+import requests
+import time
+from datetime import date
+
+mod = Blueprint('auth', __name__)
+
+
+
+# login and get token
+@app.route('/auth', methods=['POST'])
+def auth():
+    return jsonify({'status':'success','token':'136458476182639'})
+
+# logout provided account info with token
+@app.route('/logout',methods=['POST'])
+def logout():    
+    return jsonify({'status':'success','message':'successfully logged out'})
