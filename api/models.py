e3f4969a022f49c24e169864a31cef5a0b1b6f77
shreyshrey1 <talk2shreyas@gmail.com>
2017-12-03 12:42:09 -0600

    error codes

--- api/models.py
+++ api/models.py
@@ -126,7 +126,7 @@ class AdditionalLinks(db.Model):
         return {'url': self.url, 'poi_id': self.poi_id}
 
 class InvalidUsage(Exception):
-        status_code = 400
+    status_code = 400
 
     def __init__(self, message, status_code=None, payload=None):
         Exception.__init__(self)
