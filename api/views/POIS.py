1aaf1f4569ce754ef36137b1533f547dc3aa90c5
shreyshrey1 <talk2shreyas@gmail.com>
2017-12-08 15:51:00 -0600

    changes

--- api/views/POIS.py
+++ api/views/POIS.py
@@ -139,6 +139,7 @@ def poi_delete(poi_id):
             for link in json_dict['additional_links']:
                 result = AdditionalLinks(
                     url=(link['url']),
+                    url_name=(link['url_name'])
                 )
                 db.session.add(result)
             db.session.commit()
